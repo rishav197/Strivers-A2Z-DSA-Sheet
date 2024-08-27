a09437dd2f4ce30403308b7466411a6d9c034ade
rishav197 <rv1972002gmail.com>
2024-08-27 19:00:10 +0530

    one more ques added

---
+++ BinaryTree/Lec3-Hard-Problems/count-total-Nodes-in-complete-BT.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void solve(TreeNode* root, int &count){
+        //base case
+        if(root==NULL){
+            return;
+        }
+
+        count++;
+        solve(root->left,count);
+        solve(root->right,count);
+    }
+
+    int countNodes(TreeNode* root) {
+        int count=0;
+        solve(root,count);
+
+        return count;
+    }
+};
