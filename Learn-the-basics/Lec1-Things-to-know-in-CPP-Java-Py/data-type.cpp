0a683f17086529f9e9c119bf4f75ec2a42e430d4
rishav197 <rv1972002gmail.com>
2024-12-31 14:01:36 +0530

    one more ques added

---
+++ Learn-the-basics/Lec1-Things-to-know-in-CPP-Java-Py/data-type.cpp
@@ -0,0 +1,27 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    int dataTypeSize(string str) {
+        
+        if(str=="Character"){
+            return sizeof(char);
+        }
+        else if(str=="Integer"){
+            return sizeof(int);
+        }
+        else if(str=="Long"){
+            return sizeof(long);
+        }
+        else if(str=="Float"){
+            return sizeof(float);
+        }
+        else if(str=="Double"){
+            return sizeof(double);
+        }
+        else{
+            return -1;
+        }
+        
+    }
+};
