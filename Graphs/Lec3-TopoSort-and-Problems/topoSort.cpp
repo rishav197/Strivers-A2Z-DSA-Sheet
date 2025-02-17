bab3863c5ce66671f736a70a7f10f953b14670f1
rishav197 <rv1972002gmail.com>
2025-02-17 12:48:35 +0530

    small change

--- Graphs/Lec3-TopoSort-and-Problems/topoSort.cpp
+++ Graphs/Lec3-TopoSort-and-Problems/topoSort.cpp
@@ -1,45 +1,46 @@
+/**/
+
 class Solution {
-  public:
-    // Function to return list containing vertices in Topological order.
-    
-    void topoSort(int node, vector<vector<int>>& adj, unordered_map<int,bool> &visited, stack<int> &s){
-        visited[node]=true;
-        
-        //traverse neighbours
-        for(auto neighbour : adj[node]){
-            
-            if(!visited[neighbour]){
-                topoSort(neighbour, adj, visited, s);   
-            }
-        }
-        
-        //when returing back from a recursive call
-        s.push(node);
-    }
-    
-    vector<int> topologicalSort(vector<vector<int>>& adj) {
-        
-        unordered_map<int,bool> visited;
-        
-        int v = adj.size();
-        // cout<<v<<endl;
-        
-        stack<int> s;
-        
-        //calling topoSort for each vertex
-        for(int i=0; i<v; i++){
-            if(!visited[i]){
-                topoSort(i, adj, visited, s);
-            }
-        }
-        
-        vector<int> ans;
-        
-        while(!s.empty()){
-            ans.push_back(s.top());
-            s.pop();
-        }
-        
-        return ans;
-    }
-};
+    public:
+      void dfs(int node, vector<int> &vis, vector<vector<int>> &adj, stack<int> &topo){
+          //mark visited
+          vis[node]=1;
+          
+          //traverse neighbours 
+          for(auto ngr : adj[node]){
+              if(!vis[ngr]){
+                  dfs(ngr, vis, adj, topo);
+              }
+          }
+          
+          //while returning from a recursive call
+          topo.push(node);
+      }
+      
+      // Function to return list containing vertices in Topological order.
+      vector<int> topologicalSort(vector<vector<int>>& adj) {
+          
+          int v = adj.size();
+          vector<int> vis(v, 0);
+          stack<int> topo;
+          
+          for(int i=0; i<v; i++){
+              if(!vis[i]){
+                  //call dfs for each node
+                  dfs(i, vis, adj, topo);    
+              }
+          }
+          
+          //stores topo order
+          vector<int> ans;
+          
+          while(!topo.empty()){
+              auto top = topo.top();
+              topo.pop();
+              
+              ans.push_back(top);
+          }
+          
+          return ans;
+      }
+  };
