7a738303bde7eefabcfd3fa29202169e316ea54f
rishav197 <rv1972002gmail.com>
2025-02-18 14:49:18 +0530

    one more ques added

--- Graphs/Lec3-TopoSort-and-Problems/kahns-algo.cpp
+++ Graphs/Lec3-TopoSort-and-Problems/kahns-algo.cpp
@@ -1,55 +1,47 @@
 class Solution {
-  public:
-    // Function to return list containing vertices in Topological order.
-    vector<int> topologicalSort(vector<vector<int>>& adj) {
-        
-        int v = adj.size();
-        vector<int> indegree(v);
-        
-        //step1: find all indegrees
-        for(auto i : adj){
-            
-            for(auto j : i){
-                indegree[j]++;
-            }
-        }
-        
-        // for(auto i : indegree){
-        //     cout<<i<<" ";
-        // }
-        // cout<<endl;
-        
-        //step2: insert nodes into queue which has zero indegree
-        queue<int> q;
-        for(int i=0; i<v; i++){
-            
-            if(indegree[i]==0){
-                q.push(i);
-            }
-        }
-        
-        //step3: do bfs-breath first search
-        vector<int> ans;
-        while(!q.empty()){
-            int frontNode = q.front();
-            q.pop();
-            
-            //ans store
-            ans.push_back(frontNode);
-            
-            //neighbour indegree update
-            for(auto neighbour : adj[frontNode]){
-                indegree[neighbour]-=1;
-                
-                if(indegree[neighbour]==0){
-                    q.push(neighbour);
-                }
-                
-            }
-            
-        }
-        
-        
-        return ans;
-    }
-};
+    public:
+      // Function to return list containing vertices in Topological order.
+      vector<int> topologicalSort(vector<vector<int>>& adj) {
+          int v = adj.size(); //nodes
+          
+          vector<int> indegree(v, 0);
+          
+          //step1 : indegree calc for all nodes
+          for(int i=0; i<v; i++){
+              for(auto node : adj[i]){
+                  indegree[node]++;
+              }    
+          }
+          
+          //step2 : 0 degree walo ko push kardo
+          queue<int> q;
+          for(int i=0; i<v; i++){
+              if(indegree[i]==0){
+                  q.push(i);
+              }
+          }
+          
+          
+          //step3 : Do BFS
+          vector<int> ans;
+          
+          while(!q.empty()){
+              auto topNode = q.front();
+              q.pop();
+              
+              //store answer
+              ans.push_back(topNode);
+              
+              for(auto ngr : adj[topNode]){
+                  
+                  indegree[ngr]--; //decrease indeg by 1
+                  if(indegree[ngr]==0){ //check if ngr has indeg=0
+                      q.push(ngr);
+                  }
+              }
+          }
+          
+          return ans;
+          
+      }
+  };
