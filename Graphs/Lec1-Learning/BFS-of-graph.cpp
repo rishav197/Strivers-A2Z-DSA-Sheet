561109c26954f56f124bd4dc481be629011c8531
rishav197 <rv1972002gmail.com>
2025-08-06 20:58:32 +0530

    small update

--- Graphs/Lec1-Learning/BFS-of-graph.cpp
+++ Graphs/Lec1-Learning/BFS-of-graph.cpp
@@ -29,3 +29,39 @@ class Solution {
         return ans;
     }
 };
+
+
+/*------------------------------------------------ */ 
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfs(vector<vector<int>> &adj) {
+        int n = adj.size();
+        
+        queue<int> q;
+        q.push(0);
+
+        vector<bool> vis(n, 0);
+        vis[0]=1;
+        
+        vector<int> ans;
+        
+        //trsl starts here
+        while(!q.empty()){
+            
+            auto topNode = q.front();
+            q.pop();
+            ans.push_back(topNode); //storing answer
+            
+            for(auto ngr : adj[topNode]){
+                if(!vis[ngr]){ //checking if vis[ngr] visited or not
+                    q.push(ngr);
+                    vis[ngr]=1;
+                }
+            }
+        }
+        
+        return ans;
+        
+    }
+};
